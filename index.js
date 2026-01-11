5e8328da23cb987f75a7ed5f84a4dff6f9137c43
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-11 09:54:39 -0500

    Added button to adjust grid size.

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const gridSize = 16;
-const content = document.querySelector('.content');
+const gridButton = document.querySelector('#setGridSize');
+gridButton.addEventListener('click', resetGrid);
 
-const container = document.createElement('div');
-container.classList.add('container');
-container.addEventListener('mouseover', markBox);
+createGrid(16);
 
-for (let y = 0; y < gridSize; y++ ) {
-    // create row
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+function createGrid(gridSize = 16) {
+    const content = document.querySelector('.content');
 
-    // fill row with boxes
-    for (let x = 0; x < gridSize; x++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        row.appendChild(box);
+    // delete the grid if it already exists
+    const existingGrid = content.querySelector('.container');
+    if (existingGrid) {
+        existingGrid.remove();
     }
-}
 
-content.appendChild(container);
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.addEventListener('mouseover', markBox);
+
+    for (let y = 0; y < gridSize; y++ ) {
+        // create row
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+
+        // fill row with boxes
+        for (let x = 0; x < gridSize; x++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+    }
+
+    content.appendChild(container);    
+};
 
 function markBox(event) {
     if (event.target.classList.contains('box'))  {
         event.target.classList.add('marked');
     }
-}
+};
+
+function resetGrid(event) {
+    const gridSize = +(prompt('Enter a size for the grid -- Max: 100'));
+    if (typeof(gridSize) === 'number' & gridSize > 0 && gridSize <= 100) {
+        createGrid(gridSize);
+    }
+};
